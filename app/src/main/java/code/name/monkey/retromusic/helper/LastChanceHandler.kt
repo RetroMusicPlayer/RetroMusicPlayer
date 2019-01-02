0456914e2a2e266c761c07d1e83c272b2488addf
hemanths@live.com <hemanth@stacklighting.com>
2019-01-02 13:04:00 +0530

    Hmm somethings are changed

---
+++ app/src/main/java/code/name/monkey/retromusic/helper/LastChanceHandler.kt
@@ -0,0 +1,38 @@
+package code.name.monkey.retromusic.helper
+
+import android.content.Intent
+import androidx.core.app.ActivityCompat
+import code.name.monkey.retromusic.App
+import code.name.monkey.retromusic.ui.activities.ErrorHandlerActivity
+
+class TopExceptionHandler() : Thread.UncaughtExceptionHandler {
+    private val defaultUEH: Thread.UncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler()
+
+    override fun uncaughtException(t: Thread, e: Throwable) {
+        var arr = e.stackTrace
+        var report = e.toString() + "\n\n"
+        report += "--------- Stack trace ---------\n\n"
+        for (i in arr.indices) {
+            report += "    " + arr[i].toString() + "\n"
+        }
+        report += "-------------------------------\n\n"
+
+        // If the exception was thrown in a background thread inside
+        // AsyncTask, then the actual exception can be found with getCause
+
+        report += "--------- Cause ---------\n\n"
+        val cause = e.cause
+        if (cause != null) {
+            report += cause.toString() + "\n\n"
+            arr = cause.stackTrace
+            for (i in arr.indices) {
+                report += "    " + arr[i].toString() + "\n"
+            }
+        }
+        report += "-------------------------------\n\n"
+        ActivityCompat.startActivity(App.context, Intent(App.context, ErrorHandlerActivity::class.java)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                .putExtra("error", report), null)
+        defaultUEH.uncaughtException(t, e)
+    }
+}
