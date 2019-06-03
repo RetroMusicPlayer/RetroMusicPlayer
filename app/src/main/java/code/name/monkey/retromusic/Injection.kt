a271a180b50caee770263479e9bce2fd373d2349
h4h13 <hemanths@live.com>
2019-06-03 21:56:48 +0530

    Fix banner

--- app/src/main/java/code/name/monkey/retromusic/Injection.kt
+++ app/src/main/java/code/name/monkey/retromusic/Injection.kt
@@ -16,8 +16,6 @@ package code.name.monkey.retromusic
 
 import code.name.monkey.retromusic.providers.RepositoryImpl
 import code.name.monkey.retromusic.providers.interfaces.Repository
-import code.name.monkey.retromusic.rest.KogouClient
-import code.name.monkey.retromusic.rest.service.KuGouApiService
 import code.name.monkey.retromusic.util.schedulers.BaseSchedulerProvider
 import code.name.monkey.retromusic.util.schedulers.SchedulerProvider
 
