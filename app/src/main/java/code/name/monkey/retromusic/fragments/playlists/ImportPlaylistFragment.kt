8cebb0c603656ecbc5781ccae48d314dff6ed5e2
Hemanth S <hemanths@live.com>
2020-09-07 13:34:51 +0530

    Code refactor

--- app/src/main/java/code/name/monkey/retromusic/fragments/playlists/ImportPlaylistFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/playlists/ImportPlaylistFragment.kt
@@ -1,25 +1,25 @@
 package code.name.monkey.retromusic.fragments.playlists
 
 import android.os.Bundle
-import android.view.*
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
 import android.widget.Toast
-import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import code.name.monkey.retromusic.R
-import code.name.monkey.retromusic.adapter.base.MediaEntryViewHolder
+import code.name.monkey.retromusic.adapter.playlist.LegacyPlaylistAdapter
 import code.name.monkey.retromusic.db.PlaylistEntity
 import code.name.monkey.retromusic.fragments.ReloadType.Playlists
 import code.name.monkey.retromusic.fragments.base.AbsRecyclerViewFragment
 import code.name.monkey.retromusic.model.Playlist
-import code.name.monkey.retromusic.util.MusicUtil
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
 
 class ImportPlaylistFragment :
-    AbsRecyclerViewFragment<LegacyPlaylistAdapter, LinearLayoutManager>(), PlaylistClickListener {
+    AbsRecyclerViewFragment<LegacyPlaylistAdapter, LinearLayoutManager>(),
+    LegacyPlaylistAdapter.PlaylistClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -69,48 +69,4 @@ class ImportPlaylistFragment :
         menu.removeItem(R.id.action_sort_order)
         super.onCreateOptionsMenu(menu, inflater)
     }
-}
-
-class LegacyPlaylistAdapter(
-    private val activity: FragmentActivity,
-    private var list: List<Playlist>,
-    private val layoutRes: Int,
-    private val playlistClickListener: PlaylistClickListener
-) :
-    RecyclerView.Adapter<LegacyPlaylistAdapter.ViewHolder>() {
-
-    fun swapData(list: List<Playlist>) {
-        this.list = list
-        notifyDataSetChanged()
-    }
-
-    class ViewHolder(itemView: View) : MediaEntryViewHolder(itemView) {
-
-    }
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): ViewHolder {
-        return ViewHolder(
-            LayoutInflater.from(parent.context).inflate(layoutRes, parent, false)
-        )
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val playlist: Playlist = list[position]
-        holder.title?.text = playlist.name
-        holder.text?.text = MusicUtil.getPlaylistInfoString(activity, playlist.getSongs())
-        holder.itemView.setOnClickListener {
-            playlistClickListener.onPlaylistClick(playlist)
-        }
-    }
-
-    override fun getItemCount(): Int {
-        return list.size
-    }
-}
-
-interface PlaylistClickListener {
-    fun onPlaylistClick(playlist: Playlist)
 }
