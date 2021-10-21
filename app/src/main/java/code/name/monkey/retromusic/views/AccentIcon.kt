68787f4f0842e8c66ba11870a54ab4d40fb43215
Hemanth S <hemanths@live.com>
2021-10-21 23:21:19 +0530

    Text and color changes

--- app/src/main/java/code/name/monkey/retromusic/views/AccentIcon.kt
+++ app/src/main/java/code/name/monkey/retromusic/views/AccentIcon.kt
@@ -4,7 +4,13 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
+import code.name.monkey.appthemehelper.ThemeStore
+import code.name.monkey.appthemehelper.util.VersionUtils
+import code.name.monkey.retromusic.App
+import code.name.monkey.retromusic.R
 import code.name.monkey.retromusic.extensions.accentColor
+import code.name.monkey.retromusic.util.PreferenceUtil
 
 class AccentIcon @JvmOverloads constructor(
     context: Context,
@@ -12,6 +18,11 @@ class AccentIcon @JvmOverloads constructor(
     defStyleAttr: Int = -1
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
     init {
-        imageTintList = ColorStateList.valueOf(accentColor())
+        val color = if (PreferenceUtil.materialYou && VersionUtils.hasS()) {
+            ContextCompat.getColor(context, R.color.m3_accent_color)
+        } else {
+            ThemeStore.accentColor(context)
+        }
+        imageTintList = ColorStateList.valueOf(color)
     }
 }
