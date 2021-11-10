5ee9c049248b10539f4a30effefc29f2f191ab91
Prathamesh More <prathameshmm02@gmail.com>
2021-11-10 17:33:33 +0530

    Fixed normal player appearing behind navbar

--- app/src/main/java/code/name/monkey/retromusic/fragments/player/normal/PlayerFragment.kt
+++ app/src/main/java/code/name/monkey/retromusic/fragments/player/normal/PlayerFragment.kt
@@ -24,6 +24,7 @@ import code.name.monkey.appthemehelper.util.ATHUtil
 import code.name.monkey.appthemehelper.util.ToolbarContentTintHelper
 import code.name.monkey.retromusic.R
 import code.name.monkey.retromusic.databinding.FragmentPlayerBinding
+import code.name.monkey.retromusic.extensions.drawAboveSystemBars
 import code.name.monkey.retromusic.fragments.base.AbsPlayerFragment
 import code.name.monkey.retromusic.fragments.player.PlayerAlbumCoverFragment
 import code.name.monkey.retromusic.helper.MusicPlayerRemote
@@ -120,7 +121,7 @@ class PlayerFragment : AbsPlayerFragment(R.layout.fragment_player) {
         _binding = FragmentPlayerBinding.bind(view)
         setUpSubFragments()
         setUpPlayerToolbar()
-        //playerToolbar().drawAboveNavBar()
+        playerToolbar().drawAboveSystemBars()
     }
 
     override fun onDestroyView() {
