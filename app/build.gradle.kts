30111f0a25bde1f07335bc88d6a6d524707ae4cc
Hemanth Savarala <hemanths@live.com>
2026-08-13 22:07:04 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         }
 
         applicationId = namespace
-        versionCode = 10702
-        versionName = "7.0.2"
+        versionCode = 10703
+        versionName = "7.0.3"
 
         buildConfigField("String", "GOOGLE_PLAY_LICENSING_KEY", "\"${getProperty(getProperties("../public.properties"), "GOOGLE_PLAY_LICENSE_KEY")}\"")
     }
